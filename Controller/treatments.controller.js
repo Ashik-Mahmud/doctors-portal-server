2d86686d04534e9ad60a67227d9e5cd76a61991d
ashikmahmud <ashikmamud187@gmail.com>
2022-05-12 00:40:19 +0600

    created mongodb connection and

--- Controller/treatments.controller.js
+++ Controller/treatments.controller.js
@@ -1,7 +1,16 @@
 /* get all the treatments */
+const client = require("../Connection/connection");
 const data = require("./../models/data.json")
 
-const getAllTreatments = (req, res) => res.send({data})
+const getAllTreatments = async(req, res) => {
+    await client.connect();
+    const treatmentCollection = client.db('doctors-portal').collection("treatments");
+    const query = {};
+    const cursor = treatmentCollection.find(query);
+    const result = await cursor.toArray();
+    res.send({success: true, result})
+}
+
 
 
 module.exports = {getAllTreatments}
