a3e07b0339eedaa5fcdd8e56cae1e86962f984f9
ashikmahmud <ashikmamud187@gmail.com>
2022-05-13 23:50:20 +0600

    added create some of endpoints and make it MVC pattern

--- Controller/booking.controller.js
+++ Controller/booking.controller.js
@@ -1,12 +1,45 @@
+const { ObjectId } = require("mongodb");
 const client = require("../Connection/connection");
+const bookingCollection = client.db('doctors-portal').collection("bookings");
 const createBookingTreatment = async(req, res) => {
     const data = req.body;    
     await client.connect();
-    const bookingCollection = client.db('doctors-portal').collection("bookings");
     const result = await bookingCollection.insertOne(data)
     if(result.acknowledged){
         res.send({success: true, message: "Booking successfully done."})
     }
 }
+const getCurrentUserBooking = async(req, res) => {
+    await client.connect();
+    const decodedId = req.decoded.uid;
+    const userId = req.query.uid;
+    if(decodedId === userId){
+        const query = {"author.uid": userId}
+        const cursor = bookingCollection.find(query);
+        const result = await cursor.toArray();
+        res.send({success: true, result});
+    }else{
+        res.status(403).send({success: false, message: "Forbidden Request."})
+    }    
+    
+}
 
-module.exports = {createBookingTreatment}
+const deleteCurrentUserBooking = async(req, res) => {
+    await client.connect();
+    const decodedId = req.decoded.uid;
+    const userId = req.query.uid;
+    const deletedId = req.query.id;
+    
+    if(decodedId === userId){
+        const query = {"_id": ObjectId(deletedId)}
+        const result = await bookingCollection.deleteOne(query)
+        if(result.acknowledged){
+            res.send({success: true, message: "deleted successfully done."});
+        }
+    }else{
+        res.status(403).send({success: false, message: "Forbidden Request."})
+    }    
+    
+}
+
+module.exports = {createBookingTreatment, getCurrentUserBooking, deleteCurrentUserBooking}
--- routes/booking.route.js
+++ routes/booking.route.js
@@ -1,7 +1,10 @@
 
 const router = require("express").Router();
-const { createBookingTreatment } = require("../Controller/booking.controller");
+const { createBookingTreatment, getCurrentUserBooking, deleteCurrentUserBooking } = require("../Controller/booking.controller");
+const VerifyToken = require("../VerifyToken/VerifyToken");
 /* post create booking */
 router.post("/", createBookingTreatment)
+router.get("/", VerifyToken, getCurrentUserBooking)
+router.delete("/", VerifyToken, deleteCurrentUserBooking)
 
 module.exports = router;
